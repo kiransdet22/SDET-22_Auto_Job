992ad15a57671ccb136ea56a01f2ac3408e89a05
KIRAN <KIRAN@DESKTOP-ADVM252>
2021-10-19 13:58:42 +0530

    pushed

---
+++ crm_maven_project/src/test/java/com/crm/contactTest/createcontactTest.java
@@ -0,0 +1,20 @@
+package com.crm.contactTest;
+
+import org.testng.annotations.Test;
+
+public class createcontactTest 
+{
+	@Test
+	public void createcontactTest() 
+	{
+		String url=System.getProperty("url");
+		System.out.println("==========passed========="+url);
+	System.out.println("execute createcontact");
+	}
+	
+
+	public void editTest() 
+	{
+		System.out.println("execute createcontact");
+	}
+}
---
+++ crm_maven_project/src/test/java/com/crm/orgtest/orgtest.java
@@ -0,0 +1,17 @@
+package com.crm.orgtest;
+
+import org.testng.annotations.Test;
+
+public class orgtest
+{
+	@Test
+	public void createogrtest() 
+	{
+		System.out.println("execute orgtest");
+	}
+	@Test
+	public void edit() 
+	{
+		System.out.println("edit org");
+	}
+}
